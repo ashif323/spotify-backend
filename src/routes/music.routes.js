a821c496f21fcc6d10b88f4f125f51ae4d042a92
ashif323 <ashifiqbal0783@gmail.com>
2026-03-04 00:09:17 +0530

    Initial commit: setup Spotify backend with authentication, music upload, album management, middleware, models, routes, and ImageKit integration

---
+++ src/routes/music.routes.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const musicController  = require('../controller/music.controller')
+const authMiddleware = require("../middleware/auth.middleware")
+const multer = require('multer')
+
+
+const upload = multer({
+    storage: multer.memoryStorage()
+})
+
+const router = express.Router()
+
+router.post("/upload", authMiddleware.authArtist, upload.single("music"), musicController.createMusic)
+
+router.post("/album", authMiddleware.authArtist, musicController.createAlbum)
+
+router.get("/", authMiddleware.authUser, musicController.getAllMusics)
+
+router.get("/albums", authMiddleware.authUser, musicController.getAllAlbum)
+
+router.get("/albums/:albumId", authMiddleware.authUser, musicController.getAlbumById)
+
+module.exports = router
